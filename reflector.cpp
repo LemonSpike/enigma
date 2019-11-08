9fd2f03413bf69c1f1af1cc00b6c818e131e4ded
Pranav Kasetti <prk19@ic.ac.uk>
2019-11-08 14:35:10 +0000

    Added reflector

---
+++ reflector.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include "reflector.h"
+#include "errors.h"
+#include "file_reader.h"
+
+using namespace std;
+
+int Reflector::read_reflector_config(const char *filename) {
+
+  ifstream input(filename);
+
+  if (input.fail()) {
+    err_stream << "Error opening reflector configuration file.";
+    return ERROR_OPENING_CONFIGURATION_FILE;
+  }
+
+  FileReader reader(err_stream);
+
+  int error_code = NO_ERROR;
+  int counter = 0;
+  while (!input.eof()) {
+    input >> ws;
+    if (input.peek() == EOF)
+      break;
+
+    int first_number = reader.read_number(input, error_code);
+    if (error_code != NO_ERROR)
+      return error_code;
+
+    input >> ws;
+    if (input.peek() == EOF) {
+      err_stream << "Incorrect number of parameters in reflector file ";
+      err_stream << "reflector.rf" << endl;
+      return INCORRECT_NUMBER_OF_REFLECTOR_PARAMETERS;
+    }
+    int second_number = reader.read_number(input, error_code);
+    if (error_code != NO_ERROR)
+      return error_code;
+
+    error_code = check_mapping(first_number, second_number);
+    if (error_code != NO_ERROR)
+      return error_code;
+    counter++;
+  }
+  if (counter < 13) {
+    err_stream << "Incorrect number of parameters in reflector file ";
+    err_stream << "reflector.rf" << endl;
+    return INCORRECT_NUMBER_OF_REFLECTOR_PARAMETERS;
+  }
+  return NO_ERROR;
+}
+
+/**
+ * This checks the mapping doesn't contain duplicate mappings before updating
+ * the mapping array.
+ * @param first_number The number to be mapped.
+ * @param second_numbr The mapped number.
+ * @return check_mapping The error code is returned.
+ */
+int Reflector::check_mapping(int first_number, int second_number) {
+  if (mapping[first_number] != -1 || first_number == second_number) {
+    err_stream << "The reflector configuration cannot have contacts mapped to ";
+    err_stream << "themselves or with more than one other contact." << endl;
+    return INVALID_REFLECTOR_MAPPING;
+  }
+  mapping[first_number] = second_number;
+  mapping[second_number] = first_number;
+  return NO_ERROR;
+}
+
+int Reflector::map_input(int input) {
+  return mapping[input];
+}
---
+++ reflector.h
@@ -0,0 +1,48 @@
+#ifndef REFLECTOR_H
+#define REFLECTOR_H
+
+#include "rotor.h"
+#include <iostream>
+using namespace std;
+
+class Reflector {
+
+  int mapping[NO_OF_LETTERS];
+  ostream &err_stream;
+
+public:
+ /**
+  * This function initialises a Reflector with the stderr stream.
+  * @param stream the stderr stream.
+  * @return Reflector The class instance.
+  */
+ Reflector(ostream &stream): err_stream(stream) {
+    for (int i = 0; i < NO_OF_LETTERS; ++i)
+      mapping[i] = -1;
+  }
+
+ /**
+  * This function reads a reflector config from a file with filename.
+  * It updates the mapping if the config is valid, and otherwise
+  * returns an error code.
+  *
+  * @param filename File path of reflector configuration.
+  * @return read_reflector_config An error code is returned. This is
+  * non-zero if error arises, or zero otherwise.
+  */
+  int read_reflector_config(const char *filename);
+
+ /**
+  * This function maps an input to another index.
+  * @param input The input to be mapped.
+  * @return map_input The mapped input.
+  */
+  int map_input(int input);
+
+private:
+  int check_mapping(int contact_one,
+		    int contact_two);
+
+};
+
+#endif
